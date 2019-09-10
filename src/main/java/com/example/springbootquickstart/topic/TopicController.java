14c110fd061c58770003f3318353a1475d7c95e0
tieunt97 <tieunt.bk97@gmail.com>
2019-09-10 23:09:58 +0700

    update topic contoller, service

--- src/main/java/com/example/springbootquickstart/topic/TopicController.java
+++ src/main/java/com/example/springbootquickstart/topic/TopicController.java
@@ -1,18 +1,43 @@
 package com.example.springbootquickstart.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
+	@Autowired
+	private TopicService topicService;
+
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("spring", "Spring Framework", "Spring Framework Description"),
-				new Topic("java", "Core Java", "Core Java Description")
-				);
+		return topicService.getAllTopics();
+	}
+
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+
+	@PostMapping("/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+
+	@PutMapping("/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@DeleteMapping("/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		 topicService.deleteTopic(id);
 	}
 }
