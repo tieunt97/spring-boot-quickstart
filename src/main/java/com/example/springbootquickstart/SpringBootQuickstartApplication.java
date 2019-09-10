d5f778581714ee23a50236d19270d5afdd6a20e9
tieunt97 <tieunt.bk97@gmail.com>
2019-09-10 17:37:13 +0700

    init

---
+++ src/main/java/com/example/springbootquickstart/SpringBootQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuickstartApplication.class, args);
+	}
+
+}
